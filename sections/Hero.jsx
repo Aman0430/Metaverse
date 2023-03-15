f7aad77e55d224a2a8dfbd9ab71e658d01f11143
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2023-03-16 02:14:06 +0530

    navbar commit

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,7 +1,12 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+
 const Hero = () => (
-  <section>
+  <section className="">
     Hero section
   </section>
 );
