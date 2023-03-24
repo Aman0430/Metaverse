14ef7cebc3203e947986f355eece51512f1f09d9
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2023-03-24 19:18:38 +0530

    feedback commit

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,59 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once: 'false', amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.5] flex justify-end flex-col lg:max-w-[370px] gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
+      >
+        <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] sm:leading-[40px] leading-36px] text-[26px] text-white">
+            Hermoini Granger
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+            Founder | Metaverse
+          </p>
+        </div>
+        <p className="mt-[20px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
+          "With the development of today's wechnology, metaverse if very useful for today's work, or can be called web 3.0 by using metaverse you can use it as anything"
+        </p>
+      </motion.div>
+
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="relative flex-1 flex justify-center items-center"
+      >
+        <img 
+          src="/planet-09.png"
+          alt="planet-09"
+          className="w-full lg:h-[600px] h-auto min-h-[210px] object-cover rounded-[38px]"
+        />
+        <motion.div
+          variants={zoomIn(0.4, 1)}
+          className="lg:block hidden absolute -left-[10%] top-[3%]"
+        >
+          <img
+            src="/stamp.png"
+            alt="stamp"
+            className="w-[155px] h-[155px] object-contain"
+          />
+        </motion.div>
+      </motion.div>
+
+    </motion.div>
   </section>
 );
 
