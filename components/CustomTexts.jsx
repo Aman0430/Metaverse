220614b56066f94a78041174d04c2a34f2b0be89
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2023-06-07 23:44:35 +0530

    final push

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,23 +1,23 @@
-'use client';
+"use client";
 
 import { motion } from "framer-motion";
 
 import { textContainer, textVariant2 } from "../utils/motion";
 
-export const TypingText = ({title, textStyles}) => (
+export const TypingText = ({ title, textStyles }) => (
   <motion.p
-  variants={textContainer}
-  className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
     {Array.from(title).map((Letter, index) => (
       <motion.span variants={textVariant2} key={index}>
-        {Letter === ' ' ? '\u00A0' : Letter}
+        {Letter === " " ? "\u00A0" : Letter}
       </motion.span>
     ))}
   </motion.p>
 );
 
-export const TitleText = ({title, textStyles}) => (
+export const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
     initial="hidden"
--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
 import { motion } from "framer-motion";
 
 import styles from "../styles";
 import { fadeIn } from "../utils/motion";
 
-const ExploreCard = ( {id, imgUrl, title, index, active, handleClick} ) => (
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
-    variants={fadeIn('right', 'spring', index*0.5, 0.75)}
+    variants={fadeIn("right", "spring", index * 0.5, 0.75)}
     className={`relative ${
-      active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
+      active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onHoverStart={() => handleClick(id)}
   >
@@ -25,18 +25,23 @@ const ExploreCard = ( {id, imgUrl, title, index, active, handleClick} ) => (
       </h3>
     ) : (
       <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
-        <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
-          <img 
+        <div
+          className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+        >
+          <img
             src="/headset.svg"
             alt="headset"
             className="w-1/2 h-1/2 object-contain"
           />
         </div>
-        <p className="font-normal text-[16px] leading-[20px] text-white">ENTER THE METAVERSE</p>
-        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">{title}</h2>
+        <p className="font-normal text-[16px] leading-[20px] text-white">
+          ENTER THE METAVERSE
+        </p>
+        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+          {title}
+        </h2>
       </div>
     )}
-
   </motion.div>
 );
 
